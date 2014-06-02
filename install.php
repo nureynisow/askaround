0c8a6702849c0e045b682d9e93fdd023be8262e1
nureyni sow <nureynisow@github.com>
2014-06-02 16:10:46 +0200

    install.php done

--- install.php
+++ install.php
@@ -1,7 +1,89 @@
 <?php
-	include 'inc_bdd.php';
-	$bdd->exec("");
-	
+session_start();
+	$bdd = null;
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost','root','');
+		$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$req = $bdd->exec('DROP DATABASE IF EXISTS `ask`');
+		//var_dump($req);
+		$req = $bdd->exec('CREATE DATABASE `ask`');
+		//var_dump($req);
+		$bdd = new PDO('mysql:host=localhost;dbname=ask','root','');
+		$req = $bdd->exec("DROP TABLE IF EXISTS `tags`;
+DROP TABLE IF EXISTS `vote`;
+DROP TABLE IF EXISTS `reponse`;
+DROP TABLE IF EXISTS `question_tag`;
+DROP TABLE IF EXISTS `question`;
+DROP TABLE IF EXISTS `user`;
 
+
+
+CREATE TABLE `user` (
+  `pseudo` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `nomcomplet` varchar(64) NOT NULL,
+  `mdp` varchar(64) NOT NULL,
+  PRIMARY KEY (`pseudo`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `question` (
+  `idQ` int(11) NOT NULL AUTO_INCREMENT,
+  `dateQ` date NOT NULL,
+  `titre` varchar(128) NOT NULL,
+  `question` text NOT NULL,
+  `pseudo` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`idQ`),
+  KEY `pseudo` (`pseudo`),
+  CONSTRAINT `question_ibfk_1` FOREIGN KEY (`pseudo`) REFERENCES `user` (`pseudo`)  
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `tags` (
+  `tag` varchar(32) NOT NULL,
+  PRIMARY KEY (`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `question_tag` (
+  `idQ` int(11) NOT NULL,
+  `tag` varchar(32) NOT NULL,
+  PRIMARY KEY (`idQ`,`tag`),
+  KEY `tag` (`tag`),
+  CONSTRAINT `question_tag_ibfk_1` FOREIGN KEY (`idQ`) REFERENCES `question` (`idQ`),
+  CONSTRAINT `question_tag_ibfk_2` FOREIGN KEY (`tag`) REFERENCES `tags` (`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `reponse` (
+  `idR` int(11) NOT NULL AUTO_INCREMENT,
+  `idQ` int(11) NOT NULL,
+  `dateR` date NOT NULL,
+  `reponse` text NOT NULL,
+  `pseudo` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`idR`),
+  KEY `idQ` (`idQ`),
+  KEY `pseudo` (`pseudo`),
+  CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`idQ`) REFERENCES `question` (`idQ`),
+  CONSTRAINT `reponse_ibfk_2` FOREIGN KEY (`pseudo`) REFERENCES `user` (`pseudo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `vote` (
+  `idrep` int(11) NOT NULL DEFAULT '0',
+  `pseudo` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idrep`,`pseudo`),
+  KEY `pseudo` (`pseudo`),
+  CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`idrep`) REFERENCES `reponse` (`idR`),
+  CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`pseudo`) REFERENCES `user` (`pseudo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+		//var_dump($req);
+	}
+	catch (Exception $e)
+	{
+			echo "<h1>ERREUR</h1>";
+	        die('Erreur : ' . $e->getMessage());
+	}
+	session_destroy();
 ?>
+<H1>Installation [finished]</h1>
 <a href="index.php">index</a>
--- nav.php
+++ nav.php
@@ -9,9 +9,44 @@
       </button>
       <a class="navbar-brand" href="index.php">Ask Around</a>
     </div>
-
+<?php if(isset($_GET['task'])){ ?>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="index.php"><span class="glyphicon glyphicon-home"><span> Home</a></li>
+        <li class="dropdown active" >
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Questions <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="index.php?task=ask">Ask Question</  a></li>
+            <li><a href="index.php?task=tags">Tags</a></li>
+            <li><a href="index.php?task=usersQ">users</a></li>
+            <li class="divider"></li>
+            <li><a href="index.php?task=mine">My Questions</a></li>
+          </ul>
+        </li>
+        <li><a target="_blank" href="http://www.labri.fr/perso/falleri/dist/ens/it103/projets/Projet2014.pdf">About</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search" method="POST" action="index.php?task=search">
+        <div class="form-group">
+          <input type="text" class="form-control" name="q" placeholder="Search a question">
+        </div>
+        <button type="submit" class="btn btn-default">Search</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="logout.php"><span class="glyphicon glyphicon-off"></span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['nomC'];?> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="index.php?task=viewprofile"><span class="glyphicon glyphicon-user"> View Profile</a></li>
+            <li><a href="index.php?task=settings"><span class="glyphicon glyphicon-wrench"> Settings</a></li>
+            <li class="divider"></li>
+            <li><a href="logout.php"><span class="glyphicon glyphicon-off"> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+    <?php }else{ ?>
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"><span> Home</a></li>
         <li class="dropdown">
@@ -45,5 +80,6 @@
         </li>
       </ul>
     </div><!-- /.navbar-collapse -->
+    <?php } ?>
   </div><!-- /.container-fluid -->
 </nav>
